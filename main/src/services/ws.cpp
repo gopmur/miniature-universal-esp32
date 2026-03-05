a4ba10a71ab39d4cacb5c1600a20c244f9445e08
Ali Jafari <ali81jafari81@gmail.com>
2026-03-05 23:33:29 +0330

    chore: refactored ws service to use new Json library

--- main/src/services/ws.cpp
+++ main/src/services/ws.cpp
@@ -1,5 +1,4 @@
 #include "services/ws.hpp"
-#include "cJSON.h"
 #include "config.hpp"
 #include "context/monitoring_data.hpp"
 #include "context/services/http.hpp"
@@ -20,7 +19,7 @@ bool WebSocketService::has_connections() {
 }
 
 void WebSocketService::main(WebSocketService* self) {
-  cJSON* root = cJSON_CreateObject();
+  Json json;
   while (true) {
     self->wait_for_notification();
 
@@ -31,14 +30,19 @@ void WebSocketService::main(WebSocketService* self) {
       }
       if (packet->header.b.type == LapplType::EOC) {
         self->queue.flush();
-        cJSON_AddNumberToObject(root, "esp32DnsServiceCpuUsage", monitoring_data.dns_service_cpu_usage);
-        cJSON_AddNumberToObject(root, "esp32LedServiceCpuUsage", monitoring_data.led_service_cpu_usage);
-        cJSON_AddNumberToObject(root, "esp32MonitorServiceCpuUsage", monitoring_data.monitor_service_cpu_usage);
-        cJSON_AddNumberToObject(root, "esp32StmUartRxServiceCpuUsage", monitoring_data.stm_uart_rx_service_cpu_usage);
-        cJSON_AddNumberToObject(root, "esp32WsServiceCpuUsage", monitoring_data.ws_service_cpu_usage);
+        json.set_number("esp32DnsServiceCpuUsage",
+                        monitoring_data.dns_service_cpu_usage);
+        json.set_number("esp32LedServiceCpuUsage",
+                        monitoring_data.led_service_cpu_usage);
+        json.set_number("esp32MonitorServiceCpuUsage",
+                        monitoring_data.monitor_service_cpu_usage);
+        json.set_number("esp32StmUartRxServiceCpuUsage",
+                        monitoring_data.stm_uart_rx_service_cpu_usage);
+        json.set_number("esp32WsServiceCpuUsage",
+                        monitoring_data.ws_service_cpu_usage);
 
-        char* json_str = cJSON_PrintUnformatted(root);
-        cJSON_Delete(root);
+        char* json_str = json.stringify();
+        json = Json();
         httpd_ws_frame_t ws_packet = {
             .final = true,
             .fragmented = false,
@@ -58,58 +62,41 @@ void WebSocketService::main(WebSocketService* self) {
           }
         }
         free(json_str);
-        root = cJSON_CreateObject();
         continue;
       }
       switch (packet->address) {
         case LapplAddress::IMU_GX:
-          cJSON_AddNumberToObject(root, "imuGx", packet->get_float());
+          json.set_number("imuGx", packet->get_float());
           break;
         case LapplAddress::IMU_GY:
-          cJSON_AddNumberToObject(root, "imuGy", packet->get_float());
+          json.set_number("imuGy", packet->get_float());
           break;
         case LapplAddress::IMU_GZ:
-          cJSON_AddNumberToObject(root, "imuGz", packet->get_float());
+          json.set_number("imuGz", packet->get_float());
           break;
         case LapplAddress::LED_SERVICE_CPU_USAGE:
-          cJSON_AddNumberToObject(root,
-                                  "ledServiceCpuUsage",
-                                  packet->get_float());
+          json.set_number("ledServiceCpuUsage", packet->get_float());
           break;
         case LapplAddress::IMU_SERVICE_CPU_USAGE:
-          cJSON_AddNumberToObject(root,
-                                  "imuServiceCpuUsage",
-                                  packet->get_float());
+          json.set_number("imuServiceCpuUsage", packet->get_float());
           break;
         case LapplAddress::MOTOR_SERVICE_CPU_USAGE:
-          cJSON_AddNumberToObject(root,
-                                  "motorServiceCpuUsage",
-                                  packet->get_float());
+          json.set_number("motorServiceCpuUsage", packet->get_float());
           break;
         case LapplAddress::SD_SERVICE_CPU_USAGE:
-          cJSON_AddNumberToObject(root,
-                                  "sdServiceCpuUsage",
-                                  packet->get_float());
+          json.set_number("sdServiceCpuUsage", packet->get_float());
           break;
         case LapplAddress::CAN_RECV_SERVICE_CPU_USAGE:
-          cJSON_AddNumberToObject(root,
-                                  "canRecvServiceCpuUsage",
-                                  packet->get_float());
+          json.set_number("canRecvServiceCpuUsage", packet->get_float());
           break;
         case LapplAddress::ESP_UART_RX_SERVICE_CPU_USAGE:
-          cJSON_AddNumberToObject(root,
-                                  "uartEspRxServiceCpuUsage",
-                                  packet->get_float());
+          json.set_number("uartEspRxServiceCpuUsage", packet->get_float());
           break;
         case LapplAddress::ESP_UART_TX_SERVICE_CPU_USAGE:
-          cJSON_AddNumberToObject(root,
-                                  "uartEspTxServiceCpuUsage",
-                                  packet->get_float());
+          json.set_number("uartEspTxServiceCpuUsage", packet->get_float());
           break;
         case LapplAddress::MONITOR_SERVICE_CPU_USAGE:
-          cJSON_AddNumberToObject(root,
-                                  "monitorServiceCpuUsage",
-                                  packet->get_float());
+          json.set_number("monitorServiceCpuUsage", packet->get_float());
           break;
         default:
           break;
